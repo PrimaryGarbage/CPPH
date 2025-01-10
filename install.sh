284d13d623dab9eb4216a6cb50a2e8ba59baad93
prim <artstudioexp@gmail.com>
2025-01-10 14:00:22 +0300

    Update install.sh script

--- install.sh
+++ install.sh
@@ -1,3 +1,21 @@
 #!/bin/sh
 
-sudo cp ./bin/cpph /usr/local/bin/
+unameOut="$(uname -s)"
+case $unameOut in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    MSYS_NT*)   machine=MSys;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
+echo "Installing for \"${machine}\""
+
+case $machine in 
+    Linux | Max | Cygwin)    sudo cp ./bin/cpph /usr/local/bin/;;
+    MinGw | MSys)            sudo cp ./bin/cpph.exe ~/bin/;;
+    *)                       echo "Unable to install for this machine. Machine: ${machine}"
+esac
+
+echo "Done"
